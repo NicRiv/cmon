0e95566483caa5a275515fcc08efe83c1cebef03
NicRiv <nicriv.dev@gmail.com>
2023-03-08 12:11:53 -0300

    -

---
+++ setup.py
@@ -0,0 +1,15 @@
+# from setuptools import setup
+
+# readme = open('./README.md', 'r')
+
+# setup(name='cmon',
+#       version='0.1.0',
+#       description='cmon is a utility that will monitor for any changes in your code and automatically restart them',
+#       long_description=readme.read(),
+#       long_description_content_type='text/markdown',
+#       author='nicriv',
+#       author_email='nicriv.dev@gmail.com',
+#       url='https://github.com/NicRiv/cmon',
+#       keywords=['utilities', 'testing', 'c', 'cpp', 'monitor'],
+#       license='MIT',
+#      )
